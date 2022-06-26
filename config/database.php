d1937142ba16dab4da3c723c48d45f3e6212d0ce
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-06-26 02:45:13 +0100

    'free-database'

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'sql4.freesqldatabase.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'swiftlogistics'),
-            'username' => env('DB_USERNAME', 'swiftlogistics'),
-            'password' => env('DB_PASSWORD', 'swiftlogistics'),
+            'database' => env('DB_DATABASE', 'sql4502167'),
+            'username' => env('DB_USERNAME', 'sql4502167'),
+            'password' => env('DB_PASSWORD', 'VUbX7fDTQR'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
